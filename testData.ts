3dd7ff077b78f0c89dcb289619febe8a043d5794
neonovna <neonovnaneona@gmail.com>
2024-05-27 12:53:29 +0200

    remove rotes and shareOptions test data

--- testData.ts
+++ testData.ts
@@ -3,11 +3,3 @@ export const routesPoints = [
   { startPoint: "Warsaw", destination: "Amsterdam" },
   { startPoint: "Warsaw", destination: "Brussels Belgium" },
 ];
-
-export const shareOptions = ["share", "send-to-phone"];
-
-export const routes = {
-  default: "en/live-map/",
-  berlinRoute:
-    "/en/live-map/directions/berlin-be-de?to=place.ChIJAVkDPzdOqEcRcDteW0YgIQQ&from=place.ChIJAZ-GmmbMHkcR_NPqiCq-8HI",
-};
